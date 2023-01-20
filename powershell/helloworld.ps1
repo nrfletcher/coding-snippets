aa39f4846186abadf623975d7608206a48e88668
Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2023-01-19 22:57:01 -0500

    Create helloworld.ps1

---
+++ powershell/helloworld.ps1
@@ -0,0 +1,13 @@
+# These are Powershell snippets of random garbage
+
+# Show all processes running
+Get-Process
+
+# Show all commands available in Powershell 
+Get-Command
+
+# Show all services running
+Get-Service
+
+# Copy some item to another area
+Copy-Item "E:\Folder1" -Destination "E:\Folder2" -Recurse
